49bef48a55922bf4f810821eea8551c96e07de78
Egorova Anna <i.annchoys@gmail.com>
2026-07-09 00:13:45 +0300

    Обновлен файл specialities.php и добавлен код в index.html для визуального контроля

--- sections/specialities.php
+++ sections/specialities.php
@@ -1,22 +1,21 @@
 <section class="specialities" id="specialities">
     <div class="container">
-        <!-- Верхний мелкий заголовок (как в Our Team) -->
+
         <div class="row">
             <div class="col-12 text-center">
                 <p class="specialities__section-title">Specialities</p>
             </div>
         </div>
 
-        <!-- Основной контент: картинка + текст -->
         <div class="row align-items-center">
-            <!-- Левая колонка: картинка (такая же ширина, как в Our Team) -->
+            
             <div class="col-lg-6 col-md-6 mb-4 mb-md-0">
                 <div class="specialities__images">
                     <img src="img/specialities/4_group.png" alt="Специальное блюдо" class="specialities__photo">
                 </div>
             </div>
 
-            <!-- Правая колонка: текст -->
+            
             <div class="col-lg-5 offset-lg-1 col-md-6 text-center">
                 <h2 class="specialities__heading">CHOCOLATE PANCAKES</h2>
                 <img src="img/specialities/3_rectangle_2.png" alt="" class="specialities__divider">
@@ -33,7 +32,7 @@
             </div>
         </div>
 
-        <!-- Точки-эллипсы внизу (три штуки) -->
+        
         <div class="row">
             <div class="col-12 text-center specialities__dots">
                 <img src="img/specialities/2_ellipse.png" alt="" class="specialities__dot">
@@ -43,7 +42,7 @@
         </div>
     </div>
 
-	 <!-- Вертикальные декоративные линии -->
+	 
 <div class="specialities__decor-lines">
     <span class="specialities__decor-line specialities__decor-line--left"></span>
     <span class="specialities__decor-line specialities__decor-line--center"></span>
